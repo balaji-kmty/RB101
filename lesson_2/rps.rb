de43ec1e70ad31c234c687ccccc302a365a9f474
Balaji Krishnamurthy <balaji-kmty@Shubhas-MacBook-Pro.local>
2019-04-26 16:41:35 -0700

    RPS code refactored

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -5,69 +5,70 @@ def prompt(message)
 end
 
 def selection_expand(pick)
-  case pick
-  when 'r'
-    'rock'
-  when 'p'
-    'paper'
-  when 's'
-    'scissors'
-  when 'l'
-    'lizard'
-  when 'S'
-    'spock'
+  selection = { 'rock': ['r', 'rock'], 'paper': ['p', 'paper'],
+                'scissors': ['s', 'scissors'], 'lizard': ['l', 'lizard'],
+                'spock': ['k', 'spock'] }
+  pick_expanded = nil
+  selection.each do |key, value|
+    pick_expanded = key.to_s if value.include?(pick)
+  end
+  if pick_expanded
+    return pick_expanded
   else
-    prompt "Invalid selection. Enter #{RPS.join(', ')}."
+    prompt "Invalid selection."\
+      " Enter (r)ock, (p)aper, (s)cissors, (l)izard, or spoc(k)."
   end
 end
 
 def user_selection
   pick = nil
   loop do
-    pick = gets.chomp
+    pick = gets.chomp.downcase
     pick = selection_expand(pick)
     break if RPS.include?(pick)
   end
   pick
 end
 
-def win(first, second)
-  rps_hash = {
+def win?(first, second)
+  win_logic = {
     'rock': ['scissors', 'lizard'],
     'paper': ['rock', 'spock'],
     'scissors': ['paper', 'lizard'],
     'lizard': ['paper', 'spock'],
     'spock': ['scissors', 'rock']
   }
-  rps_hash[first.to_sym].include?(second)
+  win_logic[first.to_sym].include?(second)
 end
 
 prompt 'Let\'s play rock/paper/scissors/lizard/spock!'
-comp_win_count = 0
+computer_win_count = 0
 user_win_count = 0
+win_count = 5
 
 loop do
-  prompt "Enter (r)ock, (p)aper, (s)cissors, (l)izard, or (S)pock."
+  prompt "Enter (r)ock, (p)aper, (s)cissors, (l)izard, or spoc(k)."
   pick = user_selection
 
   computer_pick = RPS.sample
   prompt "Computer picks #{computer_pick}"
 
-  if win(pick.downcase, computer_pick)
+  if win?(pick.downcase, computer_pick)
     prompt "#{pick.capitalize} beats #{computer_pick.capitalize}, you win!"
     user_win_count += 1
-  elsif win(computer_pick, pick)
-    prompt "#{computer_pick.capitalize} beats #{pick.capitalize}, comp wins!"
-    comp_win_count += 1
+  elsif win?(computer_pick, pick)
+    prompt "#{computer_pick.capitalize} beats #{pick.capitalize},"\
+      " computer wins!"
+    computer_win_count += 1
   else
     prompt 'It\'s a tie'
   end
 
-  prompt "Score: You #{user_win_count} Computer #{comp_win_count}"
-  break if user_win_count == 5 || comp_win_count == 5
+  prompt "Score: You #{user_win_count} Computer #{computer_win_count}"
+  break if user_win_count == win_count || computer_win_count == win_count
 end
 
-if user_win_count == 5
+if user_win_count == win_count
   prompt 'You are the grand winner, congratulations!'
 else
   prompt 'Computer is the grand winner, better luck next time!'
